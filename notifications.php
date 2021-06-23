1d7c080b546b5ef225999f04d440ee8e30bc7a95
Mauricio <Mauri@KYOPC>
2021-06-23 18:56:17 -0300

    update

--- notifications.php
+++ notifications.php
@@ -5,13 +5,24 @@ require __DIR__ .  '/vendor/autoload.php';
 
 MercadoPago\SDK::setAccessToken('APP_USR-6317427424180639-042414-47e969706991d3a442922b0702a0da44-469485398');
 
-if($_SERVER['REQUEST_METHOD'] == 'POST')
-{
-    if($_POST['type'] == 'payment'){
-        $payment = MercadoPago\Payment.find_by_id($_POST["id"]);
-        var_dump($_POST);
-    }
-    
+$body = @file_get_contents('php://input');
+$data = json_decode($body);
+file_put_contents('notificaciones/'.$data->id.".json", $body);
+
+switch($data->type) {
+    case "payment":
+        $payment = MercadoPago\Payment::find_by_id($data->data->id);
+        http_response_code(201);
+        break;
+    case "plan":
+        $plan = MercadoPago\Plan::find_by_id($data->data->id);
+        break;
+    case "subscription":
+        $plan = MercadoPago\Subscription::find_by_id($data->data->id);
+        break;
+    case "invoice":
+        $plan = MercadoPago\Invoice::find_by_id($data->data->id);
+        break;
 }
 
 ?>
